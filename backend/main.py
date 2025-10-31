fc32fcfc704088c7889d79fa7597453c2514e6ac
nicolas <nicolas.delgadop@campusuuc.edu.co>
2025-10-30 21:19:57 -0500

    Server Connection

--- backend/main.py
+++ backend/main.py
@@ -33,6 +33,8 @@ app.add_middleware(
         "http://localhost:3000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://panel-solar.vercel.app",
+        "https://panel-solar.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
